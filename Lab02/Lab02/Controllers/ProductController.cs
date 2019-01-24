59a257c39e765375f8c8e88bb609f47c32f9a83b
Brandon Ramirez <S01167199@acad.tri-c.edu>
2019-01-24 15:13:20 -0500

    Created Lab02 Project

---
+++ Lab02/Lab02/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab02.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public string Index()
+        {
+            return "Product/Index is displayed";
+        }
+    }
+}
---
+++ Lab02/Lab02/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab02.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products/Browse
+        public string Browse()
+        {
+            return "Browse displayed";
+        }
+
+        // GET: Products/Details/105
+        public string Details(int id)
+        {
+            return "Details displayed for Id=" + id;
+        }
+
+        // GET: Products/Location?zip=44124
+        public string Location(int zip)
+        {
+            return HttpUtility.HtmlEncode("Location displayed for zip=" + zip);
+        }
+    }
+}
