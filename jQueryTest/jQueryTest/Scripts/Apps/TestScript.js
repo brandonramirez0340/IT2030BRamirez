64726d25caf2cadcc31fce58cdecae025b5cc49d
Brandon Ramirez <S01167199@acad.tri-c.edu>
2019-03-21 16:43:42 -0400

    Completed Week 9 Video Walkthroughs

---
+++ jQueryTest/jQueryTest/Scripts/Apps/TestScript.js
@@ -0,0 +1,20 @@
+﻿$(function () {
+
+    // Hide button click
+    $("#hide_btn").click(function () {
+        $(".hide_content").hide();
+    });
+
+    // Show button click
+    $("#show_btn").click(function () {
+        $(".hide_content").show();
+    });
+
+    // Fun button click
+    $("#fun_btn").click(function () {
+        //$("#mainJQ").toggleClass("fun");
+
+        $("#mainJQ").find("p").css("color", "purple");
+    });
+
+});
