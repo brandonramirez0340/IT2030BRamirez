6433305a992b421dc9113bfee5efefb21136b53d
Brandon Ramirez <S01167199@acad.tri-c.edu>
2019-03-21 16:44:11 -0400

    Completed Lab 9

--- EnrollmentApplication/EnrollmentApplication/Controllers/EnrollmentController.cs
+++ EnrollmentApplication/EnrollmentApplication/Controllers/EnrollmentController.cs
@@ -14,6 +14,35 @@ namespace EnrollmentApplication.Controllers
     {
         private EnrollmentDB db = new EnrollmentDB();
 
+        public ActionResult StudentSearch(string q)
+        {
+            var students = GetStudents(q);
+            return PartialView("_StudentSearch", students);
+        }
+
+        private List<Student> GetStudents(string searchString)
+        {
+            return db.Students
+                .Where(a => a.LastName.Contains(searchString) || a.FirstName.Contains(searchString))
+                .OrderBy(a => a.LastName)
+                .ThenBy(a => a.FirstName)
+                .ToList();
+        }
+
+        public ActionResult CourseSearch(string q)
+        {
+            var courses = GetCourses(q);
+            return PartialView("_CourseSearch", courses);
+        }
+
+        private List<Course> GetCourses(string searchString)
+        {
+            return db.Courses
+                .Where(a => a.Title.Contains(searchString) || a.Description.Contains(searchString))
+                .OrderBy(a => a.Title)
+                .ToList();
+        }
+
         // GET: Enrollment
         public ActionResult Index()
         {
