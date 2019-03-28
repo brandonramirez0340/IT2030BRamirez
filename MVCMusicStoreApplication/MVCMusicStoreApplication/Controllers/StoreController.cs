0e1f258f4091ebb3e57592013aee9f4f4c07ea2f
Brandon Ramirez <S01167199@acad.tri-c.edu>
2019-03-28 19:38:43 -0400

    Completed Lab 10

---
+++ MVCMusicStoreApplication/MVCMusicStoreApplication/Controllers/StoreController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCMusicStoreApplication.Models;
+
+namespace MVCMusicStoreApplication.Controllers
+{
+    public class StoreController : Controller
+    {
+        MVCMusicStoreDB db = new MVCMusicStoreDB();
+
+        public ActionResult Index(int? id)
+        {
+            var albumsByGenre = db.Albums.Where(a => a.GenreId == id);
+            return View(albumsByGenre.ToList());
+        }
+
+        public ActionResult Browse()
+        {
+            return View(db.Genres.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            Album album = db.Albums.Find(id);
+            return View(album);
+        }
+    }
+}
